fbf097a6a9fd0790949a82c0ad1fe50a08babca4
SethVa <Sethva@vsp.com>
2017-04-07 10:39:22 -0700

    Changed

--- src/main/java/com/kalieki/sequence/Application.java
+++ src/main/java/com/kalieki/sequence/Application.java
@@ -40,11 +40,11 @@ public class Application {
     @Autowired
     public AuthenticationParameters getParameters(ApplicationConfiguration config) {
         return new AuthenticationParameters.ConfigurationBuilder()
-             .withRedirectUri(config.getRedirectHost() + config.getRedirectMapping())
+             .withRedirectUri(config.getRedirectHost())
              .withClientId(config.getClientId())
              .withClientSecret(config.getClientSecret())
-             .withState("CA")
-             .withScope("external|demo")
+             .withState("WA")
+             .withScope("demo")
              .build();
     }
 
