a7bf243f53bffdadf64a48eae035089a84501a35
SethVa <Sethva@vsp.com>
2017-04-06 21:44:50 -0700

    Added Cors

--- src/main/java/com/kalieki/sequence/shopping/DataFileController.java
+++ src/main/java/com/kalieki/sequence/shopping/DataFileController.java
@@ -1,6 +1,7 @@
 package com.kalieki.sequence.shopping;
 
 import com.kalieki.sequence.model.DataFile;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/files")
+@CrossOrigin
 public class DataFileController {
 
     public static List<DataFile> files = new ArrayList<DataFile>();
@@ -20,7 +22,7 @@ public class DataFileController {
 
     @RequestMapping(method = RequestMethod.GET)
     public List<DataFile> all(){
-        return  files;
+        return files;
 
     }
 }
